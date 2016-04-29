d7f95392b14d73290c2a23074202ca8155450dba
LAB02 <LAB02@ITPAC.LABS>
2016-04-29 19:48:49 -0300

    Add new Layout for project

---
+++ ApoioMVC/LayoutApoio/LayoutApoio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LayoutApoio.Startup))]
+namespace LayoutApoio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
